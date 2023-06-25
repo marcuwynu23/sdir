32aadf0ec7a2b0e794e94a9035cd04cb3f6428bf
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2023-06-25 18:21:33 +0800

    refactor: update and change subcommand, format,etc
    *  fix subcommand or flag handling
      - use fire module for subcommand handler
      - change flags to subcommand
    * change commands or help format
      - use tabulate for help or listing commands
    * added feature: copying directory to clipboard
      - use pyperclip for copying dir to clipboard

--- sdir.py
+++ sdir.py
@@ -1,37 +1,36 @@
-import sys
-from src.sdir import *
-help = '''
-SDIR - saved directory
-use these flags: -s,-r,-ra,-l
--s\t\tsave current directory
--r\t\tremove a directory
--ra\t\tremove all saved directory
--l\t\tlist all saved directory
-'''
-def main():
-	db = DB()
-	args = sys.argv
-	create_table_once(db)
-	try:
-		cmd = args[1]
+from lib.sdir import *
+import fire
+from tabulate import tabulate
 
-		if cmd == "-s":
-			save_dir(db)
 
-		elif cmd == "-r":
-			id = args[2]
-			remove_dir(db,id)
+def main(cmd=None, id=None):
+    db = DB()
+    create_table_once(db)
+    
+    if cmd == "save":
+        save_dir(db)
+    elif cmd == "remove" and id is not None:
+        remove_dir(db, id)
+    elif cmd == "remove-all":
+        remove_dir_all(db)
+    elif cmd == "list":
+        list_dir(db)
+    elif cmd == "clip" and id is not None:
+        copy_path_to_clip(db, id)
+    else:
+        help_table = [
+            ["Command", "Description"],
+            ["save", "Save the current directory."],
+            ["remove [id]", "Remove a saved directory by its ID."],
+            ["remove-all", "Remove all saved directories."],
+            ["list", "List all saved directories."],
+            ["clip [id]", "Copy the path of a saved directory to the clipboard."]
+        ]
+        print("SDIR - Saved Directory")
+        print(tabulate(help_table, headers="firstrow", tablefmt="plain"))
+        print("\nAuthor: Mark Wayne Menorca")
+        print("GitHub: https://github.com/marcuwynu23/sdir")
 
-		elif cmd == "-ra":
-			remove_dir_all(db)
-
-		elif cmd == "-l":
-			list_dir(db)
-		else:
-			print(help)
-
-	except IndexError as err:
-		print(help)
 
 if __name__ == '__main__':
-	main()
+    fire.Fire(main)
